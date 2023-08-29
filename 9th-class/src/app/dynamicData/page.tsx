ef5875683c6d63ccf6971b7d14f2099745c3b858
Masab Bin Zia <masabmbz5@gmail.com>
2023-08-29 23:10:10 +0500

    9th-class

---
+++ 9th-class/src/app/dynamicData/page.tsx
@@ -0,0 +1,20 @@
+async function getData() {
+    const res = await fetch("https://api.quotable.io/random?tags=technology", {
+      cache: "no-store",
+    });
+  
+    // Recommendation: handle errors
+    if (!res.ok) {
+      // This will activate the closest `error.js` Error Boundary
+      throw new Error("Failed to fetch data");
+    }
+  
+    return res.json();
+  }
+  
+  
+  export default async function Home() {
+    const quote = await getData();
+  
+    return <div>{quote.content}</div>;
+  }
---
+++ 9th-class/src/app/page.tsx
@@ -0,0 +1,12 @@
+const getData = async () => {
+  const res = await fetch("https://api.quotable.io/random?tags=technology");
+  return res.json();
+};
+
+export default async function Home() {
+  const quote = await getData();
+
+  console.log("quote: ", quote);
+
+  return <div>{quote.content}</div>;
+}
